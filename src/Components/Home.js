88bc5ec8918f7a77a2e93797a3bbd6938df9fba3
Joe Simko <joseph.simko@protonmail.com>
2023-05-17 12:04:27 -0400

    Typo Correction

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <div className='home'>
       <h1 className='title'>About</h1>
-      <h2 className='about'>Hi! My name is Joe Simko and I am Northern Virginia-based software engineer. I have experience with web development and creating a full stack application. Most of the work I'e done is with React.js and Python, but I am always happy to learn a new language or framework. </h2>
+      <h2 className='about'>Hi! My name is Joe Simko and I am Northern Virginia-based software engineer. I have experience with web development and creating a full stack application. Most of the work I've done is with React.js and Python, but I am always happy to learn a new language or framework. </h2>
       <div className='links'>
         <a href={Resume} target='_blank' rel='noreferrer'>Resume</a>
         <span className='divider'> </span>
